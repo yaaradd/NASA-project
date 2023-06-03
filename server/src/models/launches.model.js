6e1d2b6b4b2d453f735f7997fa18c00a8e01b733
Yaara <yaaradd@gmail.com>
2023-06-03 20:47:20 +0300

    add new launch and abort launch completed

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,11 +1,13 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
     flightNumber: 100,
     mission: 'kepler Exploration X',
     rocket: 'Explorer IS1',
     launchDate: new Date('December 27, 2030'),
-    destination: 'kepler-442 b',
+    target: 'kepler-442 b',
     customer: ['Yaara', 'NASA'],
     upcoming: true,
     success: true,
@@ -13,10 +15,37 @@ const launch = {
 
 launches.set(launch.flightNumber, launch);
 
+function existLaunchWithId(launchId) {
+    return launches.has(launchId);
+}
+
 function getAllLaunches() {
     return Array.from(launches.values());
+};
+
+function addNewLaunch(launch) {
+    latestFlightNumber++;
+    launches.set(
+        latestFlightNumber,
+        Object.assign(launch, {
+            success: true,
+            upcoming: true,
+            customer: ['Yaara', 'NASA'],
+            flightNumber: latestFlightNumber,
+        })
+    );
+}
+
+function abortLaunchById(launchId) {
+    const aborted = launches.get(launchId);
+    aborted.upcoming = false;
+    aborted.success = false;
+    return aborted;
 }
 
 module.exports = {
+    existLaunchWithId,
     getAllLaunches,
+    addNewLaunch,
+    abortLaunchById
 }
--- server/src/routes/launches/launches.router.js
+++ server/src/routes/launches/launches.router.js
@@ -1,11 +1,17 @@
 const express = require('express');
 
 const {
-    httpGetAllLaunches
+    httpGetAllLaunches,
+    httpAddNewLaunch,
+    httpAbortLaunch
 } = require('./launches.controller');
 
 const launchesRouter = express.Router();
 
-launchesRouter.get('/launches', httpGetAllLaunches);
+launchesRouter.get('/', httpGetAllLaunches);
+
+launchesRouter.post('/', httpAddNewLaunch);
+
+launchesRouter.delete('/:id', httpAbortLaunch);
 
 module.exports = launchesRouter;
